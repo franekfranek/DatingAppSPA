a7fd8b8435fe67692334ce210861127f7a536f19
franekfranek <franciszekezawadzki@gmail.com>
2020-04-26 13:48:34 +0200

    login/logout methods added to nav component/ ngIf condition added to html

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { error } from 'protractor';
 
 @Component({
   selector: 'app-nav', //USE THIS TO DISPLAY THE CONTENT OF COMPONENT ON OUR PAGE
@@ -9,14 +11,30 @@ export class NavComponent implements OnInit {
   model: any = {};
 
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
 
-  login() {
-    console.log(this.model);
+  login() {//always subscribe to observable(when it appears)
+    this.authService.login(this.model).subscribe(next => {
+      console.log("success");
+
+    }, err => {
+      console.log("Failed to login");
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+
+    return !!token; //token = (token != 0) ? true : false
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log("logged out");
   }
 
 }
