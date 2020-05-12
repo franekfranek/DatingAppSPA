58640adf6f53f79069db47ba5db9f66bab3d1053
franekfranek <franciszekezawadzki@gmail.com>
2020-05-12 18:02:08 +0200

    routes guard for loggedIn users added

--- src/app/_services/error.interceptor.ts
+++ src/app/_services/error.interceptor.ts
@@ -8,6 +8,7 @@ import { throwError } from 'rxjs';
 @Injectable()
 
 export class ErrorInterceptor implements HttpInterceptor {
+
   intercept(req: import("@angular/common/http").HttpRequest<any>,
             next: import("@angular/common/http").HttpHandler):
             import("rxjs").Observable<import("@angular/common/http").HttpEvent<any>> {
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,13 +3,22 @@ import { HomeComponent } from './home/home.component';
 import { MemeberListComponent } from './memeber-list/memeber-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'members', component: MemeberListComponent },
-  { path: 'messages', component: MessagesComponent },
-  { path: 'lists', component: ListsComponent },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', component: HomeComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemeberListComponent},
+      { path: 'messages', component: MessagesComponent },
+      { path: 'lists', component: ListsComponent },
+    ]
+  },
+
+  { path: '**', redirectTo: '', pathMatch: 'full' }
   //any that not fits above routes is mangae by 'wildcard' route
   //ORDER MATTERS HERE IF U PUT WILDCARD FIRST NO ROUTE ARE REACHABLE
 ];
