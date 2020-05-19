00fb506b67e0341b33d9702e08c3697cf3e32700
franekfranek <franciszekezawadzki@gmail.com>
2020-05-20 00:57:08 +0200

    user service : getUsers and getUser added

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
+import { User } from '../_models/user';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Authorization': 'Bearer ' + localStorage.getItem('token')
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
+    //get needs return type here as well because of return observable
+  }
+
+  getUser(id): Observable<User> {
+    return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
+  }
+}
