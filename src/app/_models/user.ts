f32ea42801ad016c8aefff7824bfaeda38d6a84b
franekfranek <franciszekezawadzki@gmail.com>
2020-05-19 23:45:32 +0200

    new models ralated to api dtos added

---
+++ src/app/_models/user.ts
@@ -0,0 +1,21 @@
+import { Photo } from './photo';
+
+export interface User {
+  id: number;
+  username: string;
+  knownAs: string;
+  age: number;
+  gender: string;
+  created: Date;
+  lastActive: Date;
+  photoUrl: string;
+  city: string;
+  country: string;
+  interests?: string;
+  introduction?: string;
+  lookingFor?: string;
+  photos?: Photo[];
+}
+//the optional properties(the one with ? elvis operator xD) always comes last after requiered ones
+//here there are 4 of them beacuse those are used for UserForDetailedDto
+//the obligatory ones are for UserForListDto
