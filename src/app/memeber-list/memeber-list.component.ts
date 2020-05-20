6c284f56f72dedef4500ad6397e326171cfb936c
franekfranek <franciszekezawadzki@gmail.com>
2020-05-20 23:45:04 +0200

    loadUsers method/html for user list added

--- src/app/memeber-list/memeber-list.component.ts
+++ src/app/memeber-list/memeber-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { error } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-memeber-list',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./memeber-list.component.css']
 })
 export class MemeberListComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(private userService: UserService, private alertify: AlertifyService ) { }
 
   ngOnInit(): void {
+    this.loadUsers();
   }
 
+
+  loadUsers() {
+    this.userService.getUsers().subscribe((users: User[]) => {
+      this.users = users;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
 }
