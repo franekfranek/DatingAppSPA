dfbacd2f6b22d7e8d19745018da21aa79ca41964
franekfranek <franciszekezawadzki@gmail.com>
2020-05-24 11:36:29 +0200

    members resolvers/ gallery to detailed view added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TabsModule  } from 'ngx-bootstrap/tabs';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from 'ngx-gallery-9';
 
 
 //------------local components
@@ -23,6 +24,11 @@ import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { AlertifyService } from './_services/alertify.service';
+import { AuthGuard } from './_guards/auth.guard';
+import { UserService } from './_services/user.service';
+import { MemberDetailsResolver } from './_resolvers/member-details-resolver';
+import { MemberListResolver } from './_resolvers/member-list-resolver';
 
 
 export function tokenGetter() {
@@ -49,6 +55,7 @@ export function tokenGetter() {
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
     RouterModule.forRoot(appRoutes), //we called them like this
+    NgxGalleryModule,
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
@@ -59,7 +66,12 @@ export function tokenGetter() {
   ],
   providers: [
     AuthService,
-    ErrorInterceptorProvider
+    ErrorInterceptorProvider,
+    AlertifyService,
+    AuthGuard,
+    UserService,
+    MemberDetailsResolver,
+    MemberListResolver
   ],
   bootstrap: [AppComponent]
 })
--- src/app/members/member-detail/member-detail.component.ts
+++ src/app/members/member-detail/member-detail.component.ts
@@ -3,6 +3,7 @@ import { User } from '../../_models/user';
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery-9';
 
 @Component({
   selector: 'app-member-detail',
@@ -11,21 +12,50 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberDetailComponent implements OnInit {
   user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(private userService: UserService,
               private alertify: AlertifyService,
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.loadUser();
+    this.route.data.subscribe(data => {
+      this.user = data['user']//this is the same as in route
+    });
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+
+    this.galleryImages = this.getImages();
   }
 
-  loadUser() {              //this plus here means convertin param to int
-    this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
-      this.user = user;
-    }, error => {
-        this.alertify.error(error)
-    })
+  getImages() {
+    const imageUrls = [];
+    for (const photo of this.user.photos ) {
+      imageUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url,
+        description: photo.description
+      });
+    }
+    return imageUrls;
   }
+
+  //loadUser() {              //this plus here means convertin param to int
+  //  this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
+  //    this.user = user;
+  //  }, error => {
+  //      this.alertify.error(error)
+  //  })
+  //}
 
 }
